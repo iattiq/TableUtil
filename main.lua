d8ff9556833cfdc41a360d881a2fe3c08a5b8767
Nihai <nihai_s@outlook.com>
2024-11-25 18:01:56 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,54 @@
+local TableUtil = {}
+
+
+function TableUtil.shuffle(Table: {})
+	if typeof(Table) == "table" then
+		Random.new():Shuffle(Table)
+		return Table
+	end
+end
+
+function TableUtil:Random(Table: {}, allowCycle: false)
+	
+	Table = table.clone(Table)
+	
+	if allowCycle then
+		self.shuffle(Table)
+	end
+	
+	local NewRandom = {}
+	
+	function NewRandom.Random()
+		local Index = math.random(1, #Table)
+		return Table[Index], Index
+	end
+	
+	function NewRandom:Get()
+		
+		if allowCycle then
+			
+			if not NewRandom.CycleCache then
+				NewRandom.CycleCache = {}
+			end
+			
+			if not Table[1] then
+				Table = NewRandom.CycleCache
+				table.clear(NewRandom.CycleCache)
+			end
+			
+			local RandomValue, RandomIndex = NewRandom.Random()
+
+			table.insert(NewRandom.CycleCache, RandomValue)
+			table.remove(Table, RandomIndex)
+
+			return RandomValue
+			
+		end
+		
+	end
+	
+	return NewRandom
+	
+end
+
+return TableUtil
